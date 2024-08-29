ae731ef15d1fa77149310c9e60fed0525a5e3d6e
= <mansoorshaik2621@gmail.com>
2024-08-29 20:08:02 +0530

    Fibnosis  program

---
+++ Fibino.java
@@ -0,0 +1,11 @@
+public class Fibino {
+    public static void main(String[] args) {
+        int num = 5;
+        int sum=0;
+        for(int i=0;i<=5;i++)
+        {
+            sum = sum +i;
+        }
+        System.out.println("Fibinosis of the number is: "+sum);
+    }
+}
